17d03928c42e19c58b05ee13f81715503d444ba2
bijiaju <247636225@qq.com>
2020-01-21 11:37:23 +0800

    提交代码，修改套餐表

--- src/main/resources/mapper/create.sql
+++ src/main/resources/mapper/create.sql
@@ -22,5 +22,20 @@ CREATE TABLE `tb_user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+CREATE TABLE `tb_product` (
+                            `id` int(255) NOT NULL AUTO_INCREMENT,
+                            `name` varchar(255) DEFAULT NULL,
+                            `imagePath` varchar(255) DEFAULT NULL,
+                            `uploadPath` varchar(255) DEFAULT NULL,
+                            PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
+
 INSERT INTO `test`.`tb_user` (`id`, `name`, `nickname`, `password`) VALUES ('1', '毕洋强', 'bee', '123456');
 INSERT INTO `test`.`tb_user` (`id`, `name`, `nickname`, `password`) VALUES ('97a7ec57-9596-4be1-8cca-73a9937c441a', '赵路遥', 'lion', '123456');
+
+
+INSERT INTO `test`.`tb_product` (`id`, `name`, `imagePath`, `uploadPath`) VALUES ('1', '招财猫1', '/images/招财猫1.png', '/index3');
+
